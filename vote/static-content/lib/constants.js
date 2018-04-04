8de3760041e94c1afe8ca2d3b90049167cb8883c
EricLan <eric@TheSkyLine.local>
2018-04-04 08:33:06 -0400

    vote with react, mongo, restful api, node

---
+++ vote/static-content/lib/constants.js
@@ -0,0 +1,5 @@
+global.wwPort = 8000; // CHANGE ME
+global.wwWsPort = global.wwPort+1; // for websockets DON'T CHANGE
+// global.wwHostname = "cslinux.utm.utoronto.ca"; DON'T CHANGE
+global.wwHostname = "localhost"; // CHANGE ME
+global.wwWsURL = "ws://"+global.wwHostname+":"+global.wwWsPort; // DON'T CHANGE
